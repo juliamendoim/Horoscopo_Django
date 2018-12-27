d7f7aa8f9f30004d31c2918740499b7e56d20353
MILANESE <jmilanes@TMOVILES.COM.AR>
2018-12-27 13:50:03 -0300

    con opciones

--- horoscopo/prediccion/models.py
+++ horoscopo/prediccion/models.py
@@ -1,12 +1,53 @@
 from django.db import models
 from django import forms
 
+class TextoHoroscopo(models.Model):
+    texto = models.TextField(max_length=400)
+
 class Horoscope(models.Model):
-    signo =  models.CharField(max_length = 20)
-    palabra = models.CharField(max_length = 20)
+    ARIES = 'ARIES'
+    TAURO = 'TAURO'
+    GEMINIS = 'GEMINIS'
+    CANCER = 'CANCER'
+    LEO = 'LEO'
+    VIRGO = 'VIRGO'
+    LIBRA = 'LIBRA'
+    ESCORPIO = 'ESCORPIO'
+    OFIUCO = 'OFIUCO'
+    SAGITARIO = 'SAGITARIO'
+    CAPRICORNIO = 'CAPRICORNIO'
+    ACUARIO = 'ACUARIO'
+    PISCIS = 'PISCIS'
+    SIGN = (
+        (ARIES , 'ARIES'),
+        (TAURO , 'TAURO'),
+        (GEMINIS , 'GEMINIS'),
+        (CANCER , 'CANCER'),
+        (LEO , 'LEO'),
+        (VIRGO , 'VIRGO'),
+        (LIBRA , 'LIBRA'),
+        (ESCORPIO , 'ESCORPIO'),
+        (OFIUCO , 'OFIUCO'),
+        (SAGITARIO , 'SAGITARIO'),
+        (CAPRICORNIO , 'CAPRICORNIO'),
+        (ACUARIO , 'ACUARIO'),
+        (PISCIS , 'PISCIS'),
+    )
+    signo =  models.CharField(max_length=10, choices=SIGN, default=ARIES)
+
+    AMOR = 'AMOR'
+    TRABAJO = 'TRABAJO'
+    SALUD = 'SALUD'
+    THEME = (
+        (AMOR, 'Amor'),
+        (TRABAJO, 'Trabajo'),
+        (SALUD, 'Salud'),
+    )
+
+    palabra = models.CharField(max_length=10, choices=THEME, default=AMOR)
 
 
 
 
-    #def __str__(self):
-     #   return ('<%s => %s: %s>' % (self.__class__.__name__, self.signo, self.palabra))
+
+
